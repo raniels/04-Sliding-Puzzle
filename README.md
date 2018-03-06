3bd6d6f692d875110d73fccb01ee77df61af9cc7
raniels <35283647+raniels@users.noreply.github.com>
2018-03-06 01:20:11 -0500

    finished assignment

--- README.md
+++ README.md
@@ -1,28 +1,6 @@
 # 04-image-puzzle
 
-This is the fourth programming assignment for ILS-Z399: a sliding-puzzle game. The requirements for this assignment are as follows:
-
-* Write a Python program for playing a simple "sliding puzzle" game
-* A popular variant of the sliding puzzle game is the [15 Puzzle](https://en.wikipedia.org/wiki/15_puzzle). That would be a good place to start
-* Using pygame, draw a window and graphics inside the window
-* Capture mouse or keyboard events (or both) to enable user interaction
-* Report a winning condition if the player completes the puzzle
-* Extra credit will be awarded if the puzzle is an image rather than colored shapes (but get the shapes working first!)
-* You can assume that the user will have pygame installed on their system
-
-To get a perfect score, you will need to think creatively! Think about ways you can make this project more interesting or fun.
-
-The following may be helpful to get you started:
-
-* [PyGame Drawing Basics](https://www.cs.ucsb.edu/~pconrad/cs5nm/topics/pygame/drawing/)
-* [pygame.draw.rect()](https://www.pygame.org/docs/ref/draw.html#pygame.draw.rect)
-* [pygame.font](https://www.pygame.org/docs/ref/font.html)
-* [PyGame Tutorial: Fonts and Text](http://www.nerdparadise.com/programming/pygame/part5)
-* [PyGame Events](http://www.raspberry-pi-geek.com/Archive/2014/05/Pygame-modules-for-interactive-programs)
-* [pygame.event](https://cs.iupui.edu/~aharris/pgl/pygame-1.7.1release/docs/ref/pygame_event.html)
-* You can find some [nice colors here](https://yeun.github.io/open-color/) and their [RGB recipes here](https://yeun.github.io/open-color/ingredients.html)
-
-To turn in the assignment, submit the URL to your GitHub repository.
+This is my attempt at the the 4th assignment in ils-z 399. From what I can tell it accomplishes the objectives of the assignment aside from adding other interesting features, due to time constraints. I decided to create a class so that if I decided add more methods in the future, I would not have issues with scope. The program also simulates many clicks so that you are essentially reversing what the changes were from the original positions. This was done so that the puzzle is always solvable as there are some instances if it was completley random where it would not be.
 
 ---
 
--- puzzle.py
+++ puzzle.py
@@ -1,52 +1,82 @@
 #!/usr/bin/env python
-import sys, pygame
+import sys, pygame, random
 from square import Square
 assert sys.version_info >= (3,4), 'This script requires at least Python 3.4' 
 
-screen_size = (600,600)
-dimensions = (rows,columns) = (4,4)
-FPS = 60
-black = (0,0,0)
-#colors taken from https://yeun.github.io/open-color/
-colors = [(134,142,150),(250,82,82),(230,73,128),(190,75,219),(121,80,242),(76,110,245),(34,138,230),(21,170,191),(18,184,134),(64,192,87),(130,201,30),(250,176,5),(253,126,20),(233,236,239),(255,236,153),(163,218,255)]	
+class sliding:
+    def __init__(self):
+        self.screen_size = (600,600)
+        self.dimensions = (rows,columns) = (4,4)
+        self.FPS = 60
+        self.black = (0,0,0)
+        #colors taken from https://yeun.github.io/open-color/
+        self.colors = [(134,142,150),(250,82,82),(230,73,128),(190,75,219),(121,80,242),(76,110,245),(34,138,230),(21,170,191),(18,184,134),(64,192,87),(130,201,30),(250,176,5),(253,126,20),(233,236,239),(255,236,153),(163,218,255)]
+        self.shuffle = 5000
+        self.columns = 4
+        self.rows = 4
+        self.simulatedclick = (0, 0)
 
-def calculate_xy(pos,puzzle):
-	''' calculates which square is the target '''
-	w = width / columns
-	h = height / rows
-	to_return = (int(pos[0]//w),int(pos[1]//h))
-	return to_return
 
-def main():
-	pygame.init()
-	screen = pygame.display.set_mode(screen_size)
-	font = pygame.font.SysFont("arial",64)
-	clock = pygame.time.Clock()
+    def calculate_xy(self,pos,puzzle):
+        ''' calculates which square is the target '''
+        w = 600 / self.columns
+        h = 600 / self.rows
+        to_return = (int(pos[0]//w),int(pos[1]//h))
+        return to_return
 
-	puzzle = []
-	(w,h) = (screen_size[0]/columns,screen_size[1]/rows)
-	for i in range(rows):
-		for j in range(columns):
-			position = j*rows + i
-			color = colors[position]
-			puzzle.append(Square(i,j,str(position+1),w,h,color,font))
-	
-	while True:
-		clock.tick(FPS)
+    def main(self):
+        pygame.init()
+        screen = pygame.display.set_mode(self.screen_size)
+        font = pygame.font.SysFont("arial",64)
+        clock = pygame.time.Clock()
 
-		screen.fill(black)
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()
-				sys.exit(0)
-			if event.type == pygame.MOUSEBUTTONUP:
-				pos = pygame.mouse.get_pos()
+        puzzle = []
+        (w,h) = (self.screen_size[0]/self.columns,self.screen_size[1]/self.rows)
+        for i in range(self.rows):
+            for j in range(self.columns):
+                position = j*self.rows + i
+                color = self.colors[position]
+                puzzle.append(Square(i,j,str(position+1),w,h,color,font))
 
-		for p in puzzle:
-			p.draw_square(pygame.draw,screen)		
+        puzzle[15].color = (0,0,0)
+        square16 = puzzle[15]
 
-		
-		pygame.display.flip()
+
+
+        for s in range(self.shuffle):
+            simulatedclick = (random.randrange(600),random.randrange(600))
+            simulatedmove = self.calculate_xy(simulatedclick, puzzle)
+            for p in puzzle:
+                if p.position == simulatedmove:
+                    if square16.check_proximity(simulatedmove):
+                        p.position = square16.position
+                        square16.position = simulatedmove
+
+
+        while True:
+            clock.tick(self.FPS)
+
+            screen.fill(self.black)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit(0)
+                if event.type == pygame.MOUSEBUTTONUP:
+                    pos = pygame.mouse.get_pos()
+                    move = self.calculate_xy(pos, puzzle)
+
+                    for a in puzzle:
+                        if(a.position == move):
+                            if (square16.check_proximity(move)):
+                                a.position = square16.position
+                                square16.position = move
+
+            for p in puzzle:
+                p.draw_square(pygame.draw,screen)
+
+
+            pygame.display.flip()
 
 if __name__ == '__main__':
-	main()
+    sliding = sliding()
+    sliding.main()
